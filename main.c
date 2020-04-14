56c209787abb39f9db717868531e71dd73bf5aea
Dealer301 <63659090+Dealer301@users.noreply.github.com>
2020-04-14 09:24:19 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#define CRT_SECURE_NO WARNINGS
+
+/* Title: First Program
+Author: Kevin Krywko
+Date: 2/01/2020
+Program to Print a few lines on the screen */
+
+#include <stdio.h>
+
+int main(void)
+{
+    
+    printf("Hello, World!\n");
+    printf("This is my First real attempt at coding.\n");
+    printf("That is, if one doesn't include the acomplishmets I did at Code.Org.\n");
+    printf("Simple, but beginner's stuff that I tried out.\n");
+    return 0;
+}
